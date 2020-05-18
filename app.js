132b81fbff6a621b645ed23af5b8f3073d59fbe7
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-05-18 08:52:42 -0300

    funções de remoçao e de leitura adicionado

--- app.js
+++ app.js
@@ -1,44 +1,63 @@
-function Book(title, author, pages, year) {
+function Book(title, author, pages, year, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.year = year;
+  this.isRead = isRead;
 }
 
 let myLibrary = JSON.parse(localStorage.getItem("library") || "[]");
 
 // myLibrary = [
-//   new Book("A Game of Thrones", "George R.R. Martin", "694", "1996"),
-//   new Book("A Clash of Kings", "George R.R. Martin", "761", "1998"),
-//   new Book("A Storm of Swords", "George R.R. Martin", "973", "2000"),
-//   new Book("A Feast for Crows", "George R.R. Martin", "753", "2005"),
-//   new Book("A Dance of Dragons", "George R.R. Martin", "1.016", "2011"),
+//   new Book("A Game of Thrones", "George R.R. Martin", "694", "1996", false),
+//   new Book("A Clash of Kings", "George R.R. Martin", "761", "1998", false),
+//   new Book("A Storm of Swords", "George R.R. Martin", "973", "2000", false),
+//   new Book("A Feast for Crows", "George R.R. Martin", "753", "2005", false),
+//   new Book("A Dance of Dragons", "George R.R. Martin", "1.016", "2011", false),
 // ];
 
 function addBookToLibrary(title, author, pages, year) {
   myLibrary.push(new Book(title, author, pages, year));
+  saveToLocalStorage();
+}
+
+function saveToLocalStorage() {
   localStorage.setItem("library", JSON.stringify(myLibrary))
 }
 
 function render() {
   const books = document.querySelector(".books");
+  books.innerHTML = '';
   myLibrary.forEach((book) => {
     let divBook = document.createElement("DIV");
     let title = document.createElement("H2");
     let author = document.createElement("H2");
     let pages = document.createElement("H3");
     let year = document.createElement("H3");
+    let remove = document.createElement("IMG");
+    let position = myLibrary.indexOf(book);
+    let isRead = document.createElement("IMG");
 
-    divBook.classList.add("book");
     title.innerHTML = book.title;
     author.innerHTML = `By: ${book.author}`;
     pages.innerHTML = `Pages: ${book.pages}`;
     year.innerHTML = `Year: ${book.year}`;
+    isRead.setAttribute("value", book.isRead);
 
+    remove.src  = "./img/deleteBtn.png";
+    isRead.src = "./img/toggle-off.png";
+    isRead.classList.add("isRead");
+    isRead.setAttribute("onclick", `isRead(${position})`);
+    remove.classList.add("remove");
+    remove.setAttribute("onclick", `remove(${position})`);
+    divBook.classList.add("book");
+
+    divBook.appendChild(remove);
     divBook.appendChild(title);
     divBook.appendChild(author);
     divBook.appendChild(pages);
     divBook.appendChild(year);
+    divBook.appendChild(isRead);
 
     books.appendChild(divBook);
   });
@@ -46,6 +65,8 @@ function render() {
 
 function renderForm() {
   const form = document.querySelector(".form");
+  form.innerHTML = '';
+
   form.style.visibility = "visible";
 
   const divBook = document.createElement("DIV");
@@ -58,7 +79,11 @@ function renderForm() {
   const yearLabel = document.createElement("LABEL");
   const yearInput = document.createElement("INPUT");
   const buttonSave = document.createElement("BUTTON");
+  let close = document.createElement("IMG");
 
+  close.src  = "./img/deleteBtn.png";
+  close.classList.add("remove");
+  close.setAttribute("onclick", "closeForm()");
   divBook.classList.add("divBook");
   buttonSave.classList.add("btnSave");
 
@@ -68,6 +93,7 @@ function renderForm() {
   yearLabel.innerHTML = "Year";
   buttonSave.innerHTML = "Save";
 
+  divBook.appendChild(close)
   divBook.appendChild(titleLabel);
   divBook.appendChild(titleInput);
   divBook.appendChild(authorLabel);
@@ -89,7 +115,7 @@ function getFormInfo() {
       array.push(value.value)
   })
 
-  addBookToLibrary(array[0], array[1], array[2], array[3]);
+  addBookToLibrary(array[0], array[1], array[2], array[3], array[4]);
 }
 
 document.addEventListener("click", (e) => {
@@ -102,4 +128,21 @@ document.addEventListener("click", (e) => {
   getFormInfo();
 });
 
+function remove(position) {  
+  myLibrary.splice(position, 1);
+  saveToLocalStorage();
+  render();
+}
+
+function closeForm() {
+  const form = document.querySelector(".form");
+  form.style.visibility = "hidden"
+}
+
+function isRead(position) {
+  if(!myLibrary[position].isRead) {
+    myLibrary[position].isRead = true;
+    console.log(myLibrary[position].isRead)
+  }
+}
 render();
